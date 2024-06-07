0bdf434b06fe060f45322642e506f70e548ed308
Andrew Baumann <ab@ab.id.au>
2024-06-07 23:41:08 +0200

    more-or-less functional GUI

--- VerbMeister_Blitz.py
+++ VerbMeister_Blitz.py
@@ -1,4 +1,5 @@
 import csv
+import dataclasses
 from dataclasses import dataclass
 from datetime import datetime
 import random
@@ -7,26 +8,30 @@ import random
 class VerbForm:
     infinitiv: str
     form: str
+    example: str
 
 @dataclass(frozen=True)
 class Solution(VerbForm):
     verb: str
 
-@dataclass
-class MisspeltVerb:
-    verb: str
-    correct_count: int = 0
+MISSPELT_FILENAME = 'misspelt_verbs.csv'
+MISSPELT_COUNT_FIELDNAME = 'correct_count'
+MISSPELT_FIELDS = tuple(field.name for field in dataclasses.fields(VerbForm)) + (MISSPELT_COUNT_FIELDNAME,)
+
+HIGHSCORE_FILENAME = 'high_scores.csv'
+HIGHSCORE_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 class Game:
-    def __init__(self):
+    def __init__(self) -> None:
         self.problems: list[Solution]
-        self.misspelt_verbs: dict[VerbForm, MisspeltVerb]
+        self.misspelt_verbs: dict[VerbForm, int]
 
         self.load_problems()
         self.load_misspelt_verbs()
 
         # we start by repeating the misspelt verbs
-        self.initial_questions: list[VerbForm] = list(self.misspelt_verbs.keys())
+        self.initial_questions = list(self.misspelt_verbs.keys())
+        random.shuffle(self.initial_questions)
 
     def load_problems(self) -> None:
         # Load the CSV file for the game data
@@ -35,26 +40,22 @@ class Game:
             for entry in csv.DictReader(fh):
                 self.problems.append(Solution(**entry))
 
-    # Function to save misspelt verbs, shuffled
-    def save_misspelt_verbs(self, filename='misspelt_verbs.csv') -> None:
-        misspelt_items = list(self.misspelt_verbs.items())
-        random.shuffle(misspelt_items)
-        with open(filename, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(['Infinitiv', 'Form', 'Verb', 'CorrectCount'])
-            for verb, misspelt in misspelt_items:
-                writer.writerow([verb.infinitiv, verb.form, misspelt.verb, misspelt.correct_count])
+    # Function to save misspelt verbs
+    def save_misspelt_verbs(self) -> None:
+        with open(MISSPELT_FILENAME, 'w') as file:
+            writer = csv.DictWriter(file, MISSPELT_FIELDS, extrasaction='ignore')
+            writer.writeheader()
+            for verb, count in self.misspelt_verbs.items():
+                writer.writerow(dataclasses.asdict(verb) | {MISSPELT_COUNT_FIELDNAME: count})
 
     # Function to load misspelt verbs
-    def load_misspelt_verbs(self, filename='misspelt_verbs.csv') -> None:
+    def load_misspelt_verbs(self) -> None:
         self.misspelt_verbs = {}
         try:
-            with open(filename, 'r', newline='') as file:
-                reader = csv.reader(file)
-                next(reader)  # Skip header
-                for row in reader:
-                    infinitiv, form, verb, correct_count = row
-                    self.misspelt_verbs[VerbForm(infinitiv, form)] = MisspeltVerb(verb, int(correct_count))
+            with open(MISSPELT_FILENAME, 'r') as file:
+                for row in csv.DictReader(file):
+                    count = int(row.pop(MISSPELT_COUNT_FIELDNAME))
+                    self.misspelt_verbs[VerbForm(**row)] = count
         except FileNotFoundError:
             pass  # No file to load, continue with an empty dict
 
@@ -65,51 +66,73 @@ class Game:
             return random.choice(self.problems)
 
     def submit_answer(self, vf: VerbForm, player_answer: str) -> tuple[bool, set[str]]:
+        need_save = False
         correct_answers = set(p.verb for p in self.problems if p.infinitiv == vf.infinitiv and p.form == vf.form)
-        if correct := player_answer.lower() in [verb.lower() for verb in correct_answers]:
+        if is_correct := player_answer.lower() in [verb.lower() for verb in correct_answers]:
             if vf in self.misspelt_verbs:
-                if self.misspelt_verbs[vf].correct_count > 5:
+                if self.misspelt_verbs[vf] > 5:
                     del self.misspelt_verbs[vf]
                 else:
-                    self.misspelt_verbs[vf].correct_count += 1
+                    self.misspelt_verbs[vf] += 1
+                need_save = True
         else: # wrong
-            self.misspelt_verbs[vf] = MisspeltVerb('/'.join(correct_answers))
+            self.misspelt_verbs[vf] = 0
+            need_save = True
 
-        return correct, correct_answers
+        if need_save:
+            self.save_misspelt_verbs()
 
-# Function to save high scores, sorted from highest to lowest
-def save_high_scores(scores, filename='high_scores.csv'):
-    sorted_scores = sorted(scores.items(), key=lambda x: x[1][0], reverse=True)
-    with open(filename, 'w') as file:
-        for name, (score, timestamp) in sorted_scores:
-            file.write(f"{name},{score},{timestamp}\n")
+        return is_correct, correct_answers
 
-# Function to load high scores
-def load_high_scores(filename='high_scores.csv'):
-    scores = {}
-    try:
-        with open(filename, 'r') as file:
-            for line in file:
-                name, score, timestamp = line.strip().split(',')
-                scores[name] = (int(score), timestamp)
-    except FileNotFoundError:
-        pass  # No file to load, start with empty scores
-    return scores
+class HighScores:
+    def __init__(self) -> None:
+        self.scores: dict[str, tuple[int, str]]
+        self.load()
 
-# Function to parse timestamp with multiple possible formats
-def parse_timestamp(timestamp_str):
-    for fmt in ("%Y-%m-%d %H:%M:%S", "%d/%m/%Y %H:%M"):
+    # Function to save high scores, sorted from highest to lowest
+    def save(self) -> None:
+        sorted_scores = sorted(self.scores.items(), key=lambda x: x[1][0], reverse=True)
+        with open(HIGHSCORE_FILENAME, 'w') as file:
+            writer = csv.writer(file)
+            for name, (score, timestamp) in sorted_scores:
+                writer.writerow((name, score, timestamp))
+
+    # Function to load high scores
+    def load(self) -> None:
+        self.scores = {}
         try:
-            return datetime.strptime(timestamp_str, fmt)
-        except ValueError:
-            continue
-    raise ValueError(f"Timestamp format not recognized: {timestamp_str}")
+            with open(HIGHSCORE_FILENAME, 'r') as file:            
+                for name, score, timestamp in csv.reader(file):
+                    self.scores[name] = (int(score), timestamp)
+        except FileNotFoundError:
+            pass  # No file to load, start with empty scores
+
+    def update(self, name: str, score: int) -> None:
+        if name not in self.scores or self.scores[name][0] < score:
+            self.scores[name] = (score, datetime.now().strftime(HIGHSCORE_DATE_FORMAT))
+            self.save()
+
+    def format(self) -> str:
+        # Format high scores
+        max_name_length = max(len(name) for name in self.scores.keys())
+        header_format = "{:<" + str(max_name_length) + "} {:>10} {:>15}"
+        lines = [header_format.format("Name", "Punkte", "Datum")]  # Header
+
+        # Sort and limit to top 10 scores
+        top_scores = sorted(self.scores.items(), key=lambda x: x[1][0], reverse=True)[:10]
+
+        for name, (high_score, timestamp) in top_scores:
+            date = datetime.strptime(timestamp, HIGHSCORE_DATE_FORMAT).strftime("%b %d")
+            line_format = "{:<" + str(max_name_length) + "} {:>10} {:>15}"
+            lines.append(line_format.format(name, high_score, date))
+
+        return '\n'.join(lines)
 
 def main() -> None:
     game = Game()
 
     # Initialize high scores
-    high_scores = load_high_scores()
+    high_scores = HighScores()
 
     # Game setup
     lives = 3
@@ -123,10 +146,10 @@ def main() -> None:
         player_answer = ""
         while not player_answer.strip():
             print(f"\n{vf.infinitiv}")
-            player_answer = input(f"{vf.form} ").strip()
+            player_answer = input(f"{vf.form} Ez. {vf.example} ").strip()
 
         # Check the answer
-        ok, correct_verbs = game.submit_answer(player_answer)
+        ok, correct_verbs = game.submit_answer(vf, player_answer)
         if ok:
             score += 1
             print("Richtig! Punktestand:", score)
@@ -140,26 +163,10 @@ def main() -> None:
     # Check if this is a high score and handle saving
     if score > 0:
         player_name = input("Gib deinen Namen ein, um deinen Punktestand zu speichern: ")
-        current_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        high_scores[player_name] = max((score, current_timestamp), high_scores.get(player_name, (0, '')), key=lambda x: x[0])
-        save_high_scores(high_scores)
+        high_scores.update(player_name, score)
 
     # Display high scores
-    max_name_length = max(len(name) for name in high_scores.keys())
-    print("\nHigh Scores:")
-    header_format = "{:<" + str(max_name_length) + "} {:>10} {:>15}"
-    print(header_format.format("Name", "Score", "Date"))  # Header
-
-    # Sort and limit to top 10 scores
-    top_scores = sorted(high_scores.items(), key=lambda x: x[1][0], reverse=True)[:10]
-
-    for name, (high_score, timestamp) in top_scores:
-        date = parse_timestamp(timestamp).strftime("%b %d")
-        line_format = "{:<" + str(max_name_length) + "} {:>10} {:>15}"
-        print(line_format.format(name, high_score, date))
-        
-    # Save misspelt verbs at the end of the game
-    game.save_misspelt_verbs()
+    print("\nHigh Scores:" + high_scores.format())
 
 if __name__ == "__main__":
     main()
--- gui.py
+++ gui.py
@@ -1,76 +1,152 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import simpledialog, messagebox
+from typing import Optional
 
-from VerbMeister_Blitz import Game
+from VerbMeister_Blitz import Game, HighScores
 
-class Application(ttk.Frame):
-    def __init__(self, root: tk.Tk):
-        super().__init__(root, padding="3 3 12 12")
+class GameOverDialog(simpledialog.Dialog):
+    def __init__(self, score: int, parent: Optional[tk.Misc] = None) -> None:
+        self.score = score
+        super().__init__(parent, title="Spiel aus!")
+
+    def body(self, frame: tk.Frame) -> tk.Misc | None:
+        ttk.Label(frame, text="Das Spiel ist aus!").grid(column=0, row=0, columnspan=2)
+        ttk.Label(frame, text=f"Punktestand: {self.score}").grid(column=0, row=1, columnspan=2)
+        ttk.Label(frame, text="Name für High Score:").grid(column=0, row=2)
+        self.name = tk.StringVar()
+        entry = ttk.Entry(frame, width=7, textvariable=self.name)
+        entry.grid(column=1, row=2)
+        return entry
+
+    def validate(self):
+        name = self.name.get()
+        if name:
+            self.result = name
+            return True
+        else:
+            messagebox.showwarning("Name war leer")
+            return False
+
+class HighScoreDialog(simpledialog.Dialog):
+    def __init__(self, text: str, parent: Optional[tk.Misc] = None) -> None:
+        self.hstext = text
+        super().__init__(parent, title="High Scores")
+
+    def body(self, frame: tk.Frame) -> None:
+        t = tk.Text(frame, font='TkFixedFont', width=40, height=11, wrap="none")
+        t.insert('end', self.hstext)
+        t.config(state='disabled')
+        t.grid()
+
+    def buttonbox(self) -> None:
+        box = ttk.Frame(self)
+        b = ttk.Button(box, text="OK", width=10, command=self.ok, default=tk.ACTIVE)
+        b.focus()
+        b.pack(side=tk.LEFT, padx=5, pady=5)
+        box.pack()
+
+        self.bind("<Return>", self.ok)
+        self.bind("<Escape>", self.cancel)
+
+class Application(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         self.game = Game()
 
-        root.title("VerbMeister")
-
-        self.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
-        root.columnconfigure(0, weight=1)
-        root.rowconfigure(0, weight=1)
+        self.title("VerbMeister Blitz")
 
         self.verb = tk.StringVar()
+        self.verbform = tk.StringVar()
         self.question = tk.StringVar()
         self.input = tk.StringVar()
+        self.button_label = tk.StringVar()
         self.response = tk.StringVar()
         self.points = tk.IntVar(value=0)
         self.lives = tk.IntVar(value=3)
 
-        ttk.Label(self, textvariable=self.points, foreground='blue').grid(row=1, column=1, sticky=tk.NW)
-        ttk.Label(self, textvariable=self.verb).grid(row=1, column=2)
-        ttk.Label(self, textvariable=self.lives, foreground='red').grid(row=1, column=3, sticky=tk.NE)
+        headerframe = ttk.Frame(self)
+        headerframe.grid(row=0, column=0, padx=5, pady=5, sticky=tk.NSEW)
+        headerframe.grid_columnconfigure(2, weight=1)
 
-        ttk.Label(self, textvariable=self.question).grid(row=2, column=1, columnspan=2, sticky=(tk.W, tk.E))
+        ttk.Label(headerframe, text='Punkte:', font='TkHeadingFont').grid(row=0, column=0, sticky=tk.E)
+        ttk.Label(headerframe, textvariable=self.points, foreground='blue').grid(row=0, column=1, sticky=tk.W)
 
-        input_entry = ttk.Entry(self, width=7, textvariable=self.input)
-        input_entry.grid(row=2, column=3, sticky=(tk.W, tk.E))
+        ttk.Label(headerframe, text='Leben:', font='TkHeadingFont').grid(row=0, column=3, sticky=tk.E)
+        ttk.Label(headerframe, textvariable=self.lives, foreground='red').grid(row=0, column=4, sticky=tk.W)
 
-        self.response_label = ttk.Label(self, textvariable=self.response)
-        self.response_label.grid(row=3, column=1, columnspan=2)
+        bodyframe = ttk.Frame(self)
+        bodyframe.grid(row=1, column=0, padx=5, pady=5, sticky=tk.NSEW)
+        bodyframe.grid_columnconfigure([0,1], weight=1)
 
-        self.button = ttk.Button(self, text="Check", command=self.button_press)
-        self.button.grid(row=3, column=3, sticky=tk.W)
+        ttk.Label(bodyframe, textvariable=self.verb).grid(row=0, column=0, columnspan=2, padx=5, pady=2)
+        ttk.Label(bodyframe, textvariable=self.verbform, font='TkHeadingFont').grid(row=1, column=0, columnspan=2)
 
-        for child in self.winfo_children(): 
-            child.grid_configure(padx=5, pady=5)
+        ttk.Label(bodyframe, textvariable=self.question).grid(row=2, column=0, sticky=tk.E)
+        self.input_entry = ttk.Entry(bodyframe, width=7, textvariable=self.input)
+        self.input_entry.grid(row=2, column=1, sticky=tk.EW)
 
-        input_entry.focus()
-        root.bind("<Return>", self.button_press)
+        footframe = ttk.Frame(self)
+        footframe.grid(row=2, column=0, padx=5, pady=5, sticky=tk.NSEW)
+        footframe.grid_columnconfigure(0, weight=1)
+
+        self.response_label = ttk.Label(footframe, textvariable=self.response, width=20)
+        self.response_label.grid(row=0, column=0)
+
+        ttk.Button(footframe, textvariable=self.button_label, command=self.button_press, width=6).grid(row=0, column=1, sticky=tk.E, padx=5)
+
+        self.input_entry.focus()
+        self.bind("<Return>", self.button_press)
 
         self.render_question()
 
     def button_press(self, *args):
         if self.current_question:
-            ok, correct_verbs = self.game.submit_answer(self.current_question, self.input.get())
-            if ok:
-                self.points.set(self.points.get() + 1)
-                self.response.set("Richtig!")
-                self.response_label.config(foreground='green')
-            else:
-                self.lives.set(self.lives.get() - 1)
-                self.response.set(f"Falsch! {'/'.join(correct_verbs)}")
-                self.response_label.config(foreground='red')
+            if input := self.input.get():
+                ok, correct_verbs = self.game.submit_answer(self.current_question, input)
+                self.render_response(ok, correct_verbs)
 
-            self.current_question = None
-            self.button.config(text="Next")
+                if ok:
+                    self.points.set(self.points.get() + 1)
+                else:
+                    self.lives.set(self.lives.get() - 1)
+
+                self.current_question = None
+        elif self.lives.get() == 0:
+            self.game_over(self.points.get())
         else:
             self.render_question()
 
-    def render_question(self):
+    def render_question(self) -> None:
         self.current_question = self.game.get_next_question()
         self.verb.set(self.current_question.infinitiv)
-        self.question.set(self.current_question.form)
+        self.verbform.set(self.current_question.form)
+        self.question.set(self.current_question.example.removesuffix(' …'))
         self.input.set("")
+        self.input_entry.config(state='enabled')
         self.response.set("")
-        self.button.config(text="Check")
+        self.button_label.set("Eingabe")
+
+    def render_response(self, ok: bool, correct_verbs: set[str]) -> None:
+        if ok:
+            self.response.set("Richtig!")
+            self.response_label.config(foreground='green')
+        else:
+            self.response.set(f"Falsch! {'/'.join(correct_verbs)}")
+            self.response_label.config(foreground='red')
+
+        self.button_label.set("Nächste")
+        self.input_entry.config(state='disabled')
+
+    def game_over(self, score: int) -> None:
+        d = GameOverDialog(score, self)
+        if d.result:
+            hs = HighScores()
+            hs.update(d.result, score)
+            HighScoreDialog(hs.format(), self)
+        self.destroy()
+
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = Application(root)
-    root.mainloop()
+    Application().mainloop()
